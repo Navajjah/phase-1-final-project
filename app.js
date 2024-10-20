18d0c5baf59d3e736d9e7ba0f1a902b9f707905c
Debrah Muinde <debbynav645@gmail.com>
2024-10-20 13:12:48 +0300

    Adjusts files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 let search = document.querySelector(".search-bar");
 document.querySelector("#search-icon").onclick = () => {
-    search.classList.toggle("active")
+    search.classList.toggle("active");
 }
